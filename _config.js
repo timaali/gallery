2497d6c729f78106cd842cd8880e385cb8e2d93e
tima ali <timaali43@gmail.com>
2023-03-23 10:06:32 +0300

    This commit adds the correct database password

--- _config.js
+++ _config.js
@@ -2,8 +2,8 @@ var config = {}
 
 // Update to have your correct username and password
 config.mongoURI = {
-    production: 'mongodb+srv://GalleryApp:Tifaly0453@cluster0.ox2elsq.mongodb.net/?retryWrites=true&w=majority',
-    development: 'mongodb+srv://GalleryApp:Tifaly0453@cluster0.ox2elsq.mongodb.net/?retryWrites=true&w=majority',
-    test: 'ongodb+srv://GalleryApp:Tifaly0453@cluster0.ox2elsq.mongodb.net/?retryWrites=true&w=majority',
+    production: 'mongodb+srv://GalleryApp:galleryapp@cluster0.ox2elsq.mongodb.net/?retryWrites=true&w=majority',
+    development: 'mongodb+srv://GalleryApp:galleryapp@cluster0.ox2elsq.mongodb.net/?retryWrites=true&w=majority',
+    test: 'ongodb+srv://GalleryApp:galleryapp@cluster0.ox2elsq.mongodb.net/?retryWrites=true&w=majority',
 }
 module.exports = config;
