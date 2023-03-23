cd5b75ca464d1452dcdd25de9ae2a3b8e33c71bd
tima ali <timaali43@gmail.com>
2023-03-23 10:07:21 +0300

    This commit adds the correct database password on server.js file

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ let index = require('./routes/index');
 let image = require('./routes/image');
 
 // connecting the database
-let mongodb_url = 'mongodb+srv://GalleryApp:Tifaly0453@cluster0.ox2elsq.mongodb.net/?retryWrites=true&w=majority';
+let mongodb_url = 'mongodb+srv://GalleryApp:galleryapp@cluster0.ox2elsq.mongodb.net/?retryWrites=true&w=majority';
 let dbName = 'GalleryApp';
 mongoose.connect(`${mongodb_url}${dbName}`,{ useNewUrlParser: true , useUnifiedTopology: true }, (err)=>{
     if (err) console.log(err)
