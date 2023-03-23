cd4102fd56e1431e87af39e024c672d79ebeaaea
tima ali <timaali43@gmail.com>
2023-03-23 09:53:13 +0300

    This commit adds databse url on the server.js file

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ let index = require('./routes/index');
 let image = require('./routes/image');
 
 // connecting the database
-let mongodb_url = 'mongodb://localhost:27017/';
-let dbName = 'darkroom';
+let mongodb_url = 'mongodb+srv://GalleryApp:Tifaly0453@cluster0.ox2elsq.mongodb.net/?retryWrites=true&w=majority';
+let dbName = 'GalleryApp';
 mongoose.connect(`${mongodb_url}${dbName}`,{ useNewUrlParser: true , useUnifiedTopology: true }, (err)=>{
     if (err) console.log(err)
 });
